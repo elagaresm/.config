b57141bef17488f8e9d218578fb1a99bccb4d8d7
elagaresm <enmanuellagares@gmail.com>
2022-06-29 14:18:52 -0400

    add colortheme hop and luasnip folderse

---
+++ nvim/lua/config/colortheme.lua
@@ -0,0 +1,17 @@
+require('onedarkpro').load()
+
+vim.cmd [[
+  colorscheme onedarkpro
+  syntax off
+
+  " transparent background
+
+  " highlight Normal guibg=none ctermbg=none
+  " highlight LineNr guibg=none ctermbg=none
+  " highlight Folded guibg=none ctermbg=none
+  " highlight NonText guibg=none ctermbg=none
+  " highlight SpecialKey guibg=none ctermbg=none
+  " highlight VertSplit guibg=none ctermbg=none
+  " highlight SignColumn guibg=none ctermbg=none
+  " highlight EndOfBuffer guibg=none ctermbg=none
+]]

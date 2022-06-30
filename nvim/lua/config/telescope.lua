aa97056867193fad65f54a6454a37cad3354c71d
Enmanuel Lagares <enmanuellagares@gmail.com>
2022-06-30 14:09:37 -0400

    add notes of telescope ripgrep

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -15,6 +15,7 @@ telescope.setup {
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+-- needs ripgrep to work https://github.com/BurntSushi/ripgrep
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
